e1adf91cfb594d4a6d202aa9b15488e05524052a
kunal-zm <kunal.1134@zenmonk.tech>
2023-08-28 10:43:42 +0530

    abcgd

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import addMore from '../images/addMore.png'
+import camera from '../images/camera.png'
+import mpre from '../images/mpre2.png'
+import Messages from './Messages'
+import Input from './Input'
 const Chat = () => {
   return (
     <div className='chat'>
-      
+      <div className='chatInfo'>
+        <span>Kunal</span>
+        <div className='chatIcons'>
+          <img src={addMore} alt="camera"/>
+          <img src={camera} alt="AddMore"/>
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
     </div>
   )
 }
--- src/component/Input.js
+++ src/component/Input.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import attach from '../images/attach.png'
+import img from '../images/img.jpg'
 const Input = () => {
   return (
-    <div>
-      Input
+    <div className='input'>
+      <input type="text" placeholder='Type Something........'/>
+      <div className='send'> 
+        <img src={attach} alt=""/>
+        <input type="file" id="img" style={{display:'none'}}/>
+        <label htmlFor='img'>
+          <img src={img} alt=""/>
+        </label>
+        <button>send</button>
+      </div>
     </div>
   )
 }
